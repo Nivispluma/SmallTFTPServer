125a1b7a9ebd48dc6b7f49824bbff8e10d7943e0
NivisPluma <matzeminecraft@gmail.com>
2022-07-07 12:27:56 +0200

    ADD: simple LOG system

--- mainTFTP.py
+++ mainTFTP.py
@@ -6,6 +6,8 @@ from fbtftp.base_server import BaseServer
 import os
 from os.path import exists
 
+from datetime import datetime
+
 LISTEN_ON = '192.168.178.61'
 SERVER_PORT = 69
 TFTP_ROOT = 'opt/ztp/tftproot'
@@ -58,11 +60,20 @@ def session_stats(stats):
     print('Sent Packets: {}'.format(stats.packets_sent))
     print('#' * 60)
 
+    log_time = f"{datetime.today().strftime('%Y-%m-%d')}"
+    log_ipaddress = f"{stats.peer[0]}"
+    log_ipaddress.replace(".", "/")
+    print(log_ipaddress)
+
+    with open(f"log/{log_time}-{log_ipaddress}", "w") as current_log_file:
+        print("Logfile created")
+    current_log_file.write(f"Peer: {stats.peer[0]}:{stats.peer[1]}")
+    current_log_file.write(f"File{stats.file_path}")
+
 
 def main():
     server = TftpServer(LISTEN_ON, SERVER_PORT, RETRIES, TIMEOUT)
     server.run()
-
 
 
 if __name__ == '__main__':
